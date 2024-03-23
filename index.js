7db51f9051c6d5e816904122b0f402fe7fc7057a
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-23 15:22:33 -0400

    Set up structure for page

---
+++ index.js
@@ -0,0 +1,7 @@
+const yearsId = document.getElementById('years');
+const monthsId = document.getElementById('months');
+const daysId = document.getElementById('days');
+const dayInput = document.getElementById('day');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+
