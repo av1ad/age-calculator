036175b2616d876447a09572bd391e6b4d253f30
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-23 16:00:03 -0400

    Finished logic and css for mobile design

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const yearsId = document.getElementById("years");
 const monthsId = document.getElementById("months");
 const daysId = document.getElementById("days");
-const dayInput = document.getElementById("day");
-const monthInput = document.getElementById("month");
-const yearInput = document.getElementById("year");
+const form = document.getElementById('ageForm')
 
 // Age calculator
-function ageCalculator() {
-  let today = new Date();
+function ageCalculator(e) {
+  e.preventDefault();
+  const dayInputValue = document.getElementById("day").value;
+  const monthInputValue = document.getElementById("month").value;
+  const yearInputValue = document.getElementById("year").value;
+
+  const birthDate = new Date(yearInputValue, monthInputValue - 1, dayInputValue); 
+  const today = new Date();
+
+  let years = today.getFullYear() - birthDate.getFullYear();
+  let months = today.getMonth() - birthDate.getMonth();
+  let days = today.getDate() - birthDate.getDate();
+
+  // if(months < 0 || (months === 0 && days < 0) ) {
+  //   years--;
+  //   months += 12;
+  //  }
+
+  //  if (days < 0) {
+  //   const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 0);
+  //   days += lastMonth.getDate();
+  //   months--;
+  //  }
+
+   yearsId.innerHTML = years;
+   monthsId.innerHTML = months;
+   daysId.innerHTML = days;
 }
+
+form.addEventListener('submit', ageCalculator)
