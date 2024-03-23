d98e0936bc7f92c86cc750bfb8861c1b427524c4
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-23 15:41:34 -0400

    Fixed styling a little

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const yearsId = document.getElementById('years');
-const monthsId = document.getElementById('months');
-const daysId = document.getElementById('days');
-const dayInput = document.getElementById('day');
-const monthInput = document.getElementById('month');
-const yearInput = document.getElementById('year');
+const yearsId = document.getElementById("years");
+const monthsId = document.getElementById("months");
+const daysId = document.getElementById("days");
+const dayInput = document.getElementById("day");
+const monthInput = document.getElementById("month");
+const yearInput = document.getElementById("year");
 
+// Age calculator
+function ageCalculator() {
+  let today = new Date();
+}
